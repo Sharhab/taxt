13e5c6dd84dc605944fb86ccde7ae9a73367cfb5
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-09-01 21:58:39 +0100

    Update index.ts

--- upwork/1111111/src/index.ts
+++ upwork/1111111/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import path from 'path';
-import { renderSemesterPlan, RenderData } from './semesterschedule-pdf';
+import { renderSemesterPlan, RenderData } from '../semesterschedule-pdf';
 
 const app = express();
 const port = 3000;
